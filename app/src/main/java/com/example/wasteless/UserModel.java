2f834b2ef8dfe3259301b3b88806189615aabe49
YashPitawela <ypitawela@gmail.com>
2021-09-26 01:15:32 +0530

    Yashodhara user profile commit

---
+++ app/src/main/java/com/example/wasteless/UserModel.java
@@ -0,0 +1,49 @@
+package com.example.wasteless;
+
+public class UserModel {
+    public String name, address, phoneno, email;
+
+    public UserModel() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserModel(String name, String address, String phoneno, String email) {
+        this.name = name;
+        this.address = address;
+        this.phoneno = phoneno;
+        this.email = email;
+    }
+
+}
